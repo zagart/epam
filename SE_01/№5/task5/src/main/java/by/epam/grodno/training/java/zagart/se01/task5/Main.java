6b0869f215b80e1377b0a7006685f34d100f9841
Zagrebantsev Artiom <aptos@mail.ru>
2015-10-04 19:57:59 +0300

    no message

--- SE_01/№5/task5/src/main/java/by/epam/grodno/training/java/zagart/se01/task5/Main.java
+++ SE_01/№5/task5/src/main/java/by/epam/grodno/training/java/zagart/se01/task5/Main.java
@@ -2,15 +2,25 @@ package by.epam.grodno.training.java.zagart.se01.task5;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		int dim = 5;
 		int count = 0;
+		int[][] array = new int [dim][dim];
+		
 		for (int j = 0; j < dim; j++) {
 			for (int i = 0; i < dim; i++) {
-				if (count == i || count == dim - i - 1) {
-					System.out.print("1 ");
-				} else System.out.print("0 ");
+				if (count == i || count == dim - i - 1)
+					array[j][i] = 1;
+				else 
+					array[j][i] = 0;
+				}
+				count++;
 			}
-			count++;
+		
+		for (int j = 0; j < dim; j++) {
+			for (int i = 0; i < dim; i++) {
+				System.out.print(array[j][i] + " ");
+			}
 			System.out.println();
 		}
 	}

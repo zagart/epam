9a783fdb9332d827c282557cc00792d0dc1d9c9e
Zagrebantsev Artiom <aptos@mail.ru>
2015-10-16 12:25:25 +0300

    no message

--- SE_01/№8/task8/src/main/java/by/epam/grodno/training/java/zagart/se01/task8/Main.java
+++ SE_01/№8/task8/src/main/java/by/epam/grodno/training/java/zagart/se01/task8/Main.java
@@ -3,18 +3,16 @@ package by.epam.grodno.training.java.zagart.se01.task8;
 public class Main {
 
 	public static void main(String[] args) {
-		Worker wrk = new Worker("Артем", 10, 15, 20, 25);
-		Worker wrk1 = new Worker();
-		wrk1.setName("Дмитрий");
-		wrk1.stationery.pen.setValue(20);
-		wrk1.stationery.pensil.setValue(30);
+		Stationery worker = new Stationery("Артем", 10, 15, 20);
+		Stationery worker1 = new Stationery();
+		worker1.setName("Дмитрий");
+		worker1.setEraserValue(10);
 		
-		System.out.println(wrk);
-		wrk.checkStationery();
+		System.out.println(worker);
+		worker.checkStationery();
 		System.out.println();
-		
-		System.out.println(wrk1);
-		wrk1.checkStationery();
+		System.out.println(worker1);
+		worker1.checkStationery();
 		System.out.println();
 	}
 
--- SE_01/№8/task8/src/main/java/by/epam/grodno/training/java/zagart/se01/task8/Stationery.java
+++ SE_01/№8/task8/src/main/java/by/epam/grodno/training/java/zagart/se01/task8/Stationery.java
@@ -1,21 +1,86 @@
 package by.epam.grodno.training.java.zagart.se01.task8;
 
-public class Stationery {
-	public Pen pen = new Pen();
-	public Pensil pensil = new Pensil();
-	public Eraser eraser = new Eraser();
-	public Ruler ruler = new Ruler();
+public class Stationery extends Workers {
+
+	private int penValue = 0;
+	private int pensilValue = 0;
+	private int eraserValue = 0;
 	
-	public Stationery(Pen pen, Pensil pensil, Eraser eraser, Ruler ruler) {
-		super();
-		this.pen = pen;
-		this.pensil = pensil;
-		this.eraser = eraser;
-		this.ruler = ruler;
+	public int getPenValue() {
+		return penValue;
 	}
-	
-	public Stationery() {
+
+	public void setPenValue(int penValue) {
+		this.penValue = penValue;
 	}
-	
-	
+
+	public int getPensilValue() {
+		return pensilValue;
+	}
+
+	public void setPensilValue(int pensilValue) {
+		this.pensilValue = pensilValue;
+	}
+
+	public int getEraserValue() {
+		return eraserValue;
+	}
+
+	public void setEraserValue(int eraserValue) {
+		this.eraserValue = eraserValue;
+	}
+
+		public Stationery(String name, int penValue, int pensilValue, int eraserValue) {
+			super(name);
+			this.penValue = penValue;
+			this.pensilValue = pensilValue;
+			this.eraserValue = eraserValue;
+		}
+		
+		public void checkStationery () {
+			System.out.println("У работника " + this.getName() + 
+					" есть: ");
+			if (this.getPenValue() != 0) System.out.println("- ручка;");
+			if (this.getPensilValue() != 0) System.out.println("- карандаш;");
+			if (this.getEraserValue() != 0) System.out.println("- ластик;");
+		}
+		
+		@Override
+		public String toString() {
+			return "Имя работника - " + super.getName() +
+					". Общая стоимость канцтоваров на рабочем месте - " +
+					(int) (penValue + pensilValue + eraserValue) + ".";
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = super.hashCode();
+			result = prime * result + eraserValue;
+			result = prime * result + penValue;
+			result = prime * result + pensilValue;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (!super.equals(obj))
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Stationery other = (Stationery) obj;
+			if (eraserValue != other.eraserValue)
+				return false;
+			if (penValue != other.penValue)
+				return false;
+			if (pensilValue != other.pensilValue)
+				return false;
+			return true;
+		}
+
+		public Stationery () {
+			super();
+		}
 }

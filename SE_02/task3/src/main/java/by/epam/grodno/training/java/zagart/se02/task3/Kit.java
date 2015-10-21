11fb95a64b3e46e5bb0e5148f369574a00bbd941
Zagrebantsev Artiom <aptos@mail.ru>
2015-10-21 14:56:42 +0300

    no message

--- SE_02/task3/src/main/java/by/epam/grodno/training/java/zagart/se02/task3/Kit.java
+++ SE_02/task3/src/main/java/by/epam/grodno/training/java/zagart/se02/task3/Kit.java
@@ -2,26 +2,31 @@ package by.epam.grodno.training.java.zagart.se02.task3;
 
 import java.util.LinkedList;
 
-public class Kit <CLASS extends Stationery> {
-	
+public class Kit<CLASS extends Stationery> {
+
 	LinkedList<Stationery> list = new LinkedList<Stationery>();
-	
-	public void addStationery (CLASS obj) {
+
+	public void addStationery(CLASS obj) {
 		list.add((Stationery) obj);
 	}
-	
-	public void getStationeryList() {
-		for (Stationery i : list) {
-			System.out.println(i.getName());
-		}
+
+	public LinkedList<Stationery> getStationeryList() {
+		return list;
 	}
 	
-	public void getKitCost() {
+	public void printList() {
+		for (Stationery i : list) {
+			System.out.println(i.getName() + " " + i.getCost());
+		}
+	}
+
+	public int getKitCost() {
 		int sum = 0;
 		for (Stationery i : list) {
 			sum += i.getCost();
 		}
 		System.out.println("Стоимость набора: " + sum + " руб.");
+		return sum;
 	}
 
 }

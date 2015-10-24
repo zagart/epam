cf6a9a8d1a80bb1788867ac1f7b5a1acb860a44e
Zagrebantsev Artiom <aptos@mail.ru>
2015-10-24 20:33:59 +0300

    no message

--- SE_02/task6/src/main/java/by/epam/grodno/training/java/zagart/se02/task6/Main.java
+++ SE_02/task6/src/main/java/by/epam/grodno/training/java/zagart/se02/task6/Main.java
@@ -3,13 +3,13 @@ package by.epam.grodno.training.java.zagart.se02.task6;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Submarine obj = new Submarine();
 		obj.Start();
 		obj.checkStatus();
 		obj.Stop();
 		obj.checkStatus();
-			
+
 	}
 
 }
---
+++ SE_02/task6/src/main/java/by/epam/grodno/training/java/zagart/se02/task6/SubmarineAnnotation.java
@@ -0,0 +1,8 @@
+package by.epam.grodno.training.java.zagart.se02.task6;
+
+import java.lang.annotation.Documented;
+
+@Documented
+public @interface SubmarineAnnotation {
+	String decription();
+}

da8252c968b5e711c3832535f2da505d5d1ea58c
Artiom Zagrebantsev <aptos@mail.ru>
2015-10-30 22:00:32 +0300

    no message

--- SE_04/task1/src/main/java/by/epam/grodno/training/java/zagart/se04/task1/Main.java
+++ SE_04/task1/src/main/java/by/epam/grodno/training/java/zagart/se04/task1/Main.java
@@ -1,12 +1,14 @@
 package by.epam.grodno.training.java.zagart.se04.task1;
 
 import static by.epam.grodno.training.java.zagart.se04.task1.Util.*;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 /**
  * Main class which contains enter point.
@@ -21,16 +23,23 @@ public class Main {
 		InputStream sourceFile = new FileInputStream("D:\\source.txt");
 		OutputStream resultFile = new FileOutputStream("D:\\result.txt");
 		byte[] sourceInBytes = new byte[sourceFile.available()];
+		HashMap<String, Integer> keywords = new HashMap<String, Integer>(); 
 
+		/*
+		 * Reading bytes from source file until it have them.
+		 */
 		int count = 0;
 		while (sourceFile.available() > 0) {
 			sourceFile.read(sourceInBytes);
 			count++;
 		}
-
 		resultFile.write(sourceInBytes); // Writing result to result file.
 		String fileContent = new String(sourceInBytes, StandardCharsets.UTF_8); // Source file in String.
-
+		
+		keywords = findKeywordsIterations(fileContent);
+		printKeywordsIterations(keywords);		
+		byte[] resultInBytes = new byte[keywords.size()];
+		
 	}
 
 }
--- SE_04/task1/src/main/java/by/epam/grodno/training/java/zagart/se04/task1/Util.java
+++ SE_04/task1/src/main/java/by/epam/grodno/training/java/zagart/se04/task1/Util.java
@@ -1,30 +1,49 @@
 package by.epam.grodno.training.java.zagart.se04.task1;
 
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.ResourceBundle;
 
+/**
+ * Class with static methods.
+ *
+ */
 public class Util {
-	
-	public static void iterateKeywords(String source) {
+
+	public static HashMap<String, Integer> findKeywordsIterations(String source) {
+		HashMap<String, Integer> keywords = new HashMap<String, Integer>();
 		ResourceBundle bundle = ResourceBundle.getBundle("Keywords");
 		Enumeration<?> bundleKeys = bundle.getKeys();
 		while (bundleKeys.hasMoreElements()) {
 			String key = (String) bundleKeys.nextElement();
-			findKeyword(source, bundle.getString(key));
+			key = bundle.getString(key);
+			int count = countKeywordIterations(source, key);
+			keywords.put(key, count);
 		}
-		System.out.println();
+		return keywords;
 	}
-	
-	public static int findKeyword(String source, String keyword) {
+
+	private static int countKeywordIterations(String source, String keyword) {
+		int count = 0;
 		int startIndex = 0;
-		int condition = 0;
-		int iteration = 0;
-		while (condition != -1) {
-			condition = source.indexOf(keyword, startIndex);
-			startIndex += source.indexOf(keyword, startIndex);
-			iteration += 1;
+		keyword = keyword + " ";
+		while (source.indexOf(keyword, startIndex) != -1) {
+			startIndex = source.indexOf(keyword, startIndex) + 1;
+			count++;
 		}
-		return iteration;
+		return count;
+	}
+
+	public static void printKeywordsIterations(HashMap<String, Integer> keywords) {
+		Iterator<Map.Entry<String, Integer>> itr = keywords.entrySet().iterator();
+		while (itr.hasNext()) {
+			Map.Entry<String, Integer> pair = itr.next();
+			if (pair.getValue() != 0) {
+				System.out.println(String.format("%s встречается %s раз(-a).", pair.getKey(), pair.getValue()));
+			}
+		}
 	}
 
 }
